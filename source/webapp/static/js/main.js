5dfd38aa19c4da24512d31639a2b0ee84dd40d05
Said Saliev <repsaid@gmail.com>
2023-01-31 15:54:12 +0600

    fix

--- source/webapp/static/js/main.js
+++ source/webapp/static/js/main.js
@@ -3,15 +3,15 @@
         let url = target.dataset['indexLink'];
         let response = await fetch(url);
         let index_text = await response.json();
-        let count = document.getElementById('value'+index_text.pk)
-        count.innerText = 'Лайки: '+ index_text.like
-        let icon = document.getElementById('icon'+index_text.pk)
+        let count = document.getElementById('value'+index_text.pk);
+        count.innerText = 'Лайки: '+ index_text.like;
+        let icon = document.getElementById('icon'+index_text.pk);
 
         if(index_text.result ===0){
-            icon.innerHTML = `<i class="bi bi-hand-thumbs-up"></i>`
+            icon.innerHTML = `<i class="bi bi-hand-thumbs-up"></i>`;
         }
         else {
-            icon.innerHTML = `<i class="bi bi-hand-thumbs-up-fill"></i>`
+            icon.innerHTML = `<i class="bi bi-hand-thumbs-up-fill"></i>`;
         }
     }
     async function onLoad(){
